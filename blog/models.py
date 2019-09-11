96aef7cc36aa5d089b72d5e3eeaecb2b9a503544
Darvey <darvey6@hotmail.com>
2019-09-11 00:16:58 -0700

    new path sensors page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Accelerometer(models.Model):
+    SensorID = models.IntegerField(primary_key=True)
+    x_pos = models.FloatField()
+    y_pos = models.FloatField()
+    z_pos = models.FloatField()
+    Current = models.IntegerField()
+    Voltage = models.IntegerField()
+    Temperature = models.FloatField()
+    Status = models.BooleanField()
+    UpdatedTime = models.DateTimeField(default=timezone.now)
+
+class BMS(models.Model):
+    SensorID = models.IntegerField(primary_key=True)
+    BatteryCurrent = models.FloatField()
+    BatteryTemperature = models.FloatField()
+    BatteryVoltage = models.FloatField()
+    Current = models.IntegerField()
+    Voltage = models.IntegerField()
+    Temperature = models.FloatField()
+    Status = models.BooleanField()
+    UpdatedTime = models.DateTimeField(default=timezone.now)
+
+class BoomAngle(models.Model):
+    SensorID = models.IntegerField(primary_key=True)
+    Angle = models.FloatField()
+    Current = models.IntegerField()
+    Voltage = models.IntegerField()
+    Temperature = models.IntegerField()
+    Status = models.BooleanField()
+    UpdatedTime = models.DateTimeField(default=timezone.now)
+
+class GPS(models.Model):
+    SensorID = models.IntegerField(primary_key=True)
+    Quality = models.BooleanField()
+    HDOP = models.FloatField()
+    AntennaAltitude = models.FloatField()
+    GeoidalSeparation = models.IntegerField()
+    GPRMCTimeStamp = models.DateTimeField()
+    Lat = models.FloatField()
+    Lon = models.FloatField()
+    GroundSpeed = models.FloatField()
+    TrackMadeGood = models.FloatField()
+    MagneticVariation = models.FloatField()
+    Voltage = models.IntegerField()
+    Temperature = models.IntegerField()
+    Status = models.BooleanField()
+    UpdatedTime = models.DateTimeField(default=timezone.now)
+
+class RudderMotor(models.Model):
+    SensorID = models.IntegerField(primary_key=True)
+    Current = models.IntegerField()
+    Voltage = models.IntegerField()
+    Temperature = models.IntegerField()
+    Status = models.BooleanField()
+    UpdatedTime = models.DateTimeField(default=timezone.now)
+
+class WinchMotor(models.Model):
+    SensorID = models.IntegerField(primary_key=True)
+    Current = models.IntegerField()
+    Voltage = models.IntegerField()
+    Temperature = models.IntegerField()
+    Status = models.BooleanField()
+    UpdatedTime = models.DateTimeField(default=timezone.now)
+
+class Wind(models.Model):
+    SensorID = models.IntegerField(primary_key=True)
+    WindSpeed = models.FloatField()
+    WindDirection = models.CharField(max_length=100)
+    WindReference = models.BooleanField()
+    WindTemperature = models.FloatField()
+    Current = models.IntegerField()
+    Voltage = models.IntegerField()
+    Temperature = models.IntegerField()
+    Status = models.BooleanField()
+    UpdatedTime = models.DateTimeField(default=timezone.now)
--- blog/views.py
+++ blog/views.py
@@ -16,6 +16,107 @@ posts = [
     }
 ]
 
+Winds = [
+    {
+        'SensorID': 'Test',
+        'WindSpeed': 'Test Title',
+        'WindDirection': 'Here is the content',
+        'WindReference': 'August 27, 2018',
+        'WindTemperature': 'August 27, 2018',
+        'Current': 'August 27, 2018',
+        'Voltage': 'August 27, 2018',
+        'Temperature': 'August 27, 2018',
+        'Status': 'August 27, 2018',
+        'UpdatedTime': 'August 27, 2018'
+    }
+]
+
+WinchMotor = [
+    {
+        'SensorID': 'Test',
+        'Current': 'August 27, 2018',
+        'Voltage': 'August 27, 2018',
+        'Temperature': 'August 27, 2018',
+        'Status': 'August 27, 2018',
+        'UpdatedTime': 'August 27, 2018'
+    }
+]
+
+
+RudderMotor = [
+    {
+        'SensorID': 'Test',
+        'Current': 'August 27, 2018',
+        'Voltage': 'August 27, 2018',
+        'Temperature': 'August 27, 2018',
+        'Status': 'August 27, 2018',
+        'UpdatedTime': 'August 27, 2018'
+    }
+]
+
+
+GPS = [
+    {
+        'SensorID': 'Test',
+        'Quality': 'Test Title',
+        'HDOP': 'Here is the content',
+        'AntennaAltitude': 'August 27, 2018',
+        'GeoidalSeparation': 'August 27, 2018',
+        'Lat': 'August 27, 2018',
+        'Lon': 'August 27, 2018',
+        'GroundSpeed': 'August 27, 2018',
+        'TrackMadeGood': 'August 27, 2018',
+        'MagneticVariation': 'August 27, 2018',
+        'Voltage': 'August 27, 2018',
+        'Temperature': 'August 27, 2018',
+        'Status': 'August 27, 2018',
+        'UpdatedTime': 'August 27, 2018'
+    }
+]
+
+BoomAngle = [
+    {
+        'SensorID': 'Test',
+        'Angle': 'Test Title',
+        'Current': 'August 27, 2018',
+        'Voltage': 'August 27, 2018',
+        'Temperature': 'August 27, 2018',
+        'Status': 'August 27, 2018',
+        'UpdatedTime': 'August 27, 2018'
+    }
+]
+
+BMS = [
+    {
+        'SensorID': 'Test',
+        'BatteryCurrent': 'Test Title',
+        'BatteryTemperature': 'Test Title',
+        'BatteryVoltage': 'Test Title',
+        'Current': 'August 27, 2018',
+        'Voltage': 'August 27, 2018',
+        'Temperature': 'August 27, 2018',
+        'Status': 'August 27, 2018',
+        'UpdatedTime': 'August 27, 2018'
+    },
+]
+
+Acceleratometer = [
+    {
+        'SensorID': 'Test',
+        'x_pos': 'Test Title',
+        'y_pos': 'Test Title',
+        'z_pos': 'Test Title',
+        'Current': 'August 27, 2018',
+        'Voltage': 'August 27, 2018',
+        'Temperature': 'August 27, 2018',
+        'Status': 'August 27, 2018',
+        'UpdatedTime': 'August 27, 2018'
+    },
+]
+
+
+
+
 def home(request):
     context= {
         'posts': posts
@@ -24,5 +125,11 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html')
+
+def sensors(request):
+    context={
+        'Winds': Winds
+    }
+    return render(request, 'blog/sensors.html', context)
 
 
