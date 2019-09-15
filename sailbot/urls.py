614a14ab751b30b371c62195abf180c2949ec63e
Darvey <darvey6@hotmail.com>
2019-09-15 09:54:17 -0700

    addded base html files

--- sailbot/urls.py
+++ sailbot/urls.py
@@ -4,7 +4,15 @@ from . import views
 urlpatterns = [
     path('', views.home, name='sailbot-home'),
     path('about/', views.about, name='sailbot-about'),
-    path('winchmotor/', views.winchmotor, name='sailbot-winchmotor')
+    path('wind/', views.winchmotor, name='sailbot-wind'),
+    path('winchmotor/', views.winchmotor, name='sailbot-winchmotor'),
+    path('ruddermotor/', views.winchmotor, name='sailbot-ruddermotor'),
+    path('gps/', views.winchmotor, name='sailbot-gps'),
+    path('boomangle/', views.winchmotor, name='sailbot-boomnangle'),
+    path('bms/', views.winchmotor, name='sailbot-bms'),
+    path('accelerometer/', views.winchmotor, name='sailbot-accelerometer'),
+
+
 ]
 
 
